f7a3cf45c993fcb50f15e6929840322f06b201cc
Anthony Sims <zocle@zocle.net>
2020-02-05 21:35:30 +0000

    adding 5-printf.c to 0x00-hello_world subdir

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - function using printf to output assigned string of text
+ * Return: 0
+ */
+int main(void)
+	{
+/* let's declare our string char variable. */
+	char str[51];
+
+/* let's copy the string. */
+	strcpy(str, "with proper grammar, but the outcome is a piece of art,");
+/* print it to stdout */
+	printf("%s", str);
+/* and return 0 */
+	return (0);
+}

726dcc54a0e0f51abd73639f433d84553e727742
Anthony Sims <zocle@zocle.net>
2020-02-06 02:57:06 +0000

    changed some logic, should work

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,16 +4,21 @@
 
 /**
  * main - function using write to output assigned string of text
- * Return: 0
+ * Return: 1
  */
 int main(void)
 	{
-/* let's declare our string char variable. */
+/* let's declare our int for length */
 	int strl;
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+/* and char for the string itself. */
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
 
+/* get the length of the str defined above */
 	strl = sizeof(str);
+/* write it to fd 2 */
 	write(2, str, strl);
+/* write a new line */
+	write(2, "\n", 1);
 
 /* and return 1 */
 	return (1);

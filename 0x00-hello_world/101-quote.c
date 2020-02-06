e6df7205938b3e8ff8839b64e8a043c3aead30b2
Anthony Sims <zocle@zocle.net>
2020-02-06 03:09:48 +0000

    adding 101-quote.c to 0x00-hello_world subdir

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,22 +4,20 @@
 
 /**
  * main - function using write to output assigned string of text
+ * let's declare our int for length
+ * and char for the string itself
+ * get the length of the str defined above
+ * write it to fd 2
+ * write a new line
  * Return: 1
  */
 int main(void)
 	{
-/* let's declare our int for length */
 	int strl;
-/* and char for the string itself. */
-	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-/* get the length of the str defined above */
 	strl = sizeof(str);
-/* write it to fd 2 */
 	write(2, str, strl);
-/* write a new line */
-	write(2, "\n", 1);
 
-/* and return 1 */
 	return (1);
 }

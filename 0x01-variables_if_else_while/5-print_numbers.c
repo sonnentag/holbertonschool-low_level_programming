d7b8360e37c28577289ca67e6e383e9f9408094f
Anthony Sims <zocle@zocle.net>
2020-02-07 02:26:36 +0000

    adding 5-print_numbers.c to 0x01-variables_if_else_while subdir

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit base 10 numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		printf("%d", x);
+	}
+	printf("\n");
+
+	return (0);
+}

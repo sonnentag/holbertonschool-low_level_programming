bdc8e909b4f2b7373621394b3b59f87ddeda4d83
Anthony Sims <zocle@zocle.net>
2020-02-07 01:02:36 +0000

    adding 4-print_alphabt.c to 0x01-variables_if_else_while subdir

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet except for e or q
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		if (c == 'e' || c == 'q')
+		{
+			continue;
+		}
+		putchar (c);
+	}
+	putchar ('\n');
+
+	return (0);
+}

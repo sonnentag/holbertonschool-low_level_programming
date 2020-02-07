0e83868816ce88ce0bf6ff37b23d040842d31046
Anthony Sims <zocle@zocle.net>
2020-02-07 03:35:00 +0000

    adding 8-print_base16.c to 0x01-variables_if_else_while subdir

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print 0-16 in hex 
+ * Return: 0
+ */
+int main(void)
+{
+	char n, c;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar (n);
+	}
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+	putchar ('\n');
+
+	return (0);
+}

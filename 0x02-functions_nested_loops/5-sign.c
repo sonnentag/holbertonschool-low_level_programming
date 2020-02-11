4829675658512429f96e89017913d8b0fb0638d3
Anthony Sims <zocle@zocle.net>
2020-02-11 20:18:27 +0000

    adding 5-sign.c to 0x02-functions_nested_loops subdir

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+* print_sign - print the sign of a number
+* @n: provided
+* Return: 0 for zero, 1 for positive, -1 for negative
+*/
+int print_sign(int n)
+	{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else
+	{
+		_putchar('0');
+		return (0);
+	}
+}

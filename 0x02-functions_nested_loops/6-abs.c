479af206a166ccf9f8ebfef43b80647d42f57008
Anthony Sims <zocle@zocle.net>
2020-02-11 20:53:37 +0000

    fixed indents per betty

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,19 +1,19 @@
 #include "holberton.h"
 
 /**
- *
- *
- *
+ * _abs - return absolute value of integer
+ * @n: provided
+ * Return: integer
  */
 int _abs(int n)
 {
-if(n<0)
-{
-n=-n;
-return (n);
-}
-else
-{
-return (n);
-}
+	if (n < 0)
+	{
+		n = -n;
+		return (n);
+	}
+	else
+	{
+		return (n);
+	}
 }

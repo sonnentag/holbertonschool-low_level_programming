1d3fddd9650a35681470fd48605bf74736d5f59d
Anthony Sims <zocle@zocle.net>
2020-02-11 20:07:07 +0000

    adding 4-isalpha.c to 0x02-functions_nested_loops subdir

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - check for aphabetic character
+ * @c: provided character
+ *
+ * Return: 1 for true, 0 for false
+ */
+int _isalpha(int c)
+{
+	if ((c > 65 && c < 90) || (c > 97 && c < 122))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

ec65daada01c05b2c521cf60060d92f1e14b23d0
Tony Sims <zocle@zocle.net>
2020-02-12 02:46:49 +0000

    fixed indents, spaces, and line length for if statement in 3-print_remaining_days.c

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -24,7 +24,7 @@ void print_remaining_days(int month, int day, int year)
 	}
 	else
 	{
-		if (month == 2 && day == 60 && (( year%400 == 0)|| (( year%4 == 0 ) &&( year%100 != 0))))
+		if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0)))
 		{
 			printf("Day of the year: %d\n", day);
 			printf("Remaining days: %d\n", 366 - day);

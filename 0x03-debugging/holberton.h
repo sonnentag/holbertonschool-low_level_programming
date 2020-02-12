8026d672f070a21812104fce157f4da44dc17e4e
Tony Sims <zocle@zocle.net>
2020-02-12 02:42:51 +0000

    adding 3-print_remaining_days.c and holberton.h

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 int largest_number(int,int,int);
+int convert_day(int,int);
+void print_remaining_days(int,int,int);
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */

9bfa921db53b24546d4a54011170faccbcd867bc
Tony Sims <zocle@zocle.net>
2020-02-13 21:07:15 +0000

    adding 5-more_numbers.c to 0x04-more_functions_nested_loops subdir

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * more_numbers - _putchar 0-14 ten times
+ */
+void more_numbers(void)
+{
+	int x = 0;
+	int n;
+
+	while (x <= 10)
+	{
+		for (n = 0; n <= 14; n++)
+		{
+			if (n > 9)
+			{
+				_putchar(n / 10 + '0');
+			}
+			_putchar(n % 10 + '0');
+		}
+		_putchar('\n');
+		x++;
+	}
+}
--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -16,5 +16,6 @@ void times_table(void);
 void print_times_table(int);
 void print_numbers(void);
 void print_most_numbers(void);
+void more_numbers(void);
 
 #endif

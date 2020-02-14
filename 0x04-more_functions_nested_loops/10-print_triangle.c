0620917a11872be5ab280bbc70023b24bb9a50cb
Tony Sims <zocle@zocle.net>
2020-02-14 20:30:33 +0000

    adding 10-print_triangle.c to 0x04-more_functions_nested_loops subdir

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "holberton.h"
+
+/**
+ * print_triangle - print alligned right triangle to length/width of size
+ * @size: provided
+ */
+void print_triangle(int size)
+{
+	int c;
+	int s;
+	int x;
+
+	if (size > 0)
+	{
+		for (s = 0; s < size; s++)
+		{
+			for (c = size; c > s; c--)
+			{
+				_putchar(32);
+			}
+			for (x = 0; x <= s; x++)
+			{
+				_putchar(35);
+			}
+			_putchar(10);
+		}
+
+
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}

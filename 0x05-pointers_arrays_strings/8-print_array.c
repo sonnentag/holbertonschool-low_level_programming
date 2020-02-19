c91238a45dc4d787623d424ad24ca311ea737925
Tony Sims <zocle@zocle.net>
2020-02-19 03:47:41 +0000

    adding 8-print_array.c to 0x05-pointers_arrays_strings subdir

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_array - print n elements of an array of integers
+ * @a: provided array
+ * @n: provided number of elements to print
+ */
+void print_array(int *a, int n)
+{
+	int x = 1;
+
+	printf("%d", a[0]);
+	while (x < n)
+	{
+		printf(", %d", a[x]);
+		x++;
+	}
+	printf("\n");
+}

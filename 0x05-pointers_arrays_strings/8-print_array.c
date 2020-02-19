9f92ad32067ccaf082faea1d6bcdc0e078a69558
Tony Sims <zocle@zocle.net>
2020-02-19 05:30:58 +0000

    adding 8-print_array.c to 0x05-pointers_arrays_strings subdir

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,19 +5,19 @@
  * print_array - print n elements of an array of integers
  * @a: provided array
  * @n: provided number of elements to print
- * Return: 0
  */
 void print_array(int *a, int n)
 {
 	int x = 1;
 
-	printf("%d", a[0]);
-	while (x < n)
+	if (a != NULL)
 	{
-		printf(", %d", a[x]);
+		printf("%d", a[0]);
+		while (x < n)
+		{
+			printf(", %d", a[x]);
+		}
 		x++;
 	}
 	printf("\n");
-
-	return (0);
 }

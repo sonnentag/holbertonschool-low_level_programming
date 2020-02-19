abe39f90ea3691dcd68be7745bb978895cd52197
Tony Sims <zocle@zocle.net>
2020-02-19 18:29:19 +0000

    fixed indents per betty

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,23 +1,25 @@
 #include "holberton.h"
 
 /**
- *
- *
+ * _strcpy - copy a string
+ * @src: provided source pointer
+ * @dest: provided destination [ointer
+ * Return: address of destination
  */
 char *_strcpy(char *dest, char *src)
 {
-
 	int x = 0;
-        int length = 0;
-        while (src[length] != '\0')
-        {
-                length++;
-        }
-        while (x <= length)
+	int length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+	while (x <= length)
 	{
 		dest[x] = src[x];
 		x++;
 	}
 
-return (dest);
+	return (dest);
 }

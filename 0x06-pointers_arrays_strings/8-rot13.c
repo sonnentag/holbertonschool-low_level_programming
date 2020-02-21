cb28d4f44f5d6494f16d07c11b256015e923b4aa
Tony Sims <zocle@zocle.net>
2020-02-21 03:34:16 +0000

    adding 8-rot13.c to 0x06-pointers_arrays_strings subdir

---
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * rot13 - encode a string in rot13
+ * @str: provided string
+ * Return: result
+ */
+char *rot13(char *str)
+{
+	void *ret = str;
+	char c;
+	int x;
+	char arr[2][52] = {
+		{'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+		'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D',
+		'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+		'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}, {'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+		'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+		'k', 'l', 'm', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y',
+		'Z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'}
+	};
+	while (*str)
+	{
+		c = *str;
+		for (x = 0; x < 52; x++)
+		{
+			if (arr[0][x] == c)
+			{
+				*str = arr[1][x];
+			}
+		}
+		str++;
+	}
+	return (ret);
+}

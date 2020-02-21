985683598c4c08df28f09fced965474eda43c47e
Tony Sims <zocle@zocle.net>
2020-02-21 20:35:31 +0000

    fixed long line per betty

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -19,7 +19,11 @@ char *cap_string(char *str)
 		}
 		if (str[i] >= 'a' && str[i] <= 'z')
 		{
-			if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == '\n' || str[i - 1] == '\t' || str[i - 1] == ',' || str[i - 1] == ';' || str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' || str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' || str[i - 1] == '}')
+			if (str[i - 1] == ' ' || str[i - 1] == '.' || str[i - 1] == '\n' ||
+				str[i - 1] == '\t' || str[i - 1] == ',' || str[i - 1] == ';'
+				|| str[i - 1] == '!' || str[i - 1] == '?' || str[i - 1] == '"' ||
+				str[i - 1] == '(' || str[i - 1] == ')' || str[i - 1] == '{' ||
+				str[i - 1] == '}')
 			{
 				str[i] -= 32;
 			}

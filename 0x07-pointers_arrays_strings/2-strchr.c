48f8a05c585cb43757123909d302869a39ecdc97
Tony Sims <zocle@zocle.net>
2020-02-24 20:33:54 +0000

    adding 2-strchr.c to 0x07-pointers_arrays_strings subdir

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ * _strchr - return character from string
+ * @s: string
+ * @c: character
+ * Return: remainder of string or 0
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return ((char *)s);
+		}
+		s++;
+	}
+
+	return (0);
+}

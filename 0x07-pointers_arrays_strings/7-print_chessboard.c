e4016411eaad7e4a088cf815de0a0f60321753d6
Tony Sims <zocle@zocle.net>
2020-02-25 05:39:12 +0000

    fixed comments and indents per betty

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 
 /**
- *
- *
+ * print_chessboard - print a chessboard
+ * @a: array of chesspieces
  */
 void print_chessboard(char (*a)[8])
 {
-int x, n;
-for (x = 0; x < 8; x++)
-{
-for (n = 0; n < 8; n++)
-{
-_putchar(x[a][n]);
-}
-_putchar((char)10);
-}
+	int x, n;
+
+	for (x = 0; x < 8; x++)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(x[a][n]);
+		}
+			_putchar((char)10);
+	}
 }

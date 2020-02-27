14926ff418a1bcee35ff5199384dcebbed613194
Tony Sims <zocle@zocle.net>
2020-02-26 22:11:33 -0600

    Added 6-is_prime_number.c to 0x08 subdir

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "holberton.h"
+
+/**
+ * is_prime_number - determine if prime number is given
+ * @n: given number
+ * Return: 1 if prime and 0 otherwise
+ */
+int is_prime_number(int n)
+{
+return (pnum(2, n));
+}
+
+/**
+ * pnum - helper function to determine if number is prime
+ * @i: int to increment through primes until matches or not
+ * @n: provided number to test
+ * Return: 1 if prime and 0 otherwise
+ */
+int pnum(int i, int n)
+{
+	if (n % i == 0 || n < 2)
+		return (0);
+	else if (i == n - 1)
+		return (1);
+	else if (n > i)
+		return (pnum(i + 1, n));
+	return (1);
+}
+
+

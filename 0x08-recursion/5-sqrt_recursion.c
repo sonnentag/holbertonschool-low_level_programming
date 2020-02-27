394f6d3a515c16c7475b09b99049fe007e8b6380
Tony Sims <zocle@zocle.net>
2020-02-26 22:21:15 -0600

    Fixed blank space at eol per betty

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -13,7 +13,7 @@ int _sqrt_recursion(int n)
 /**
  * square - helper function to find square root of a number
  * @p: provided number
- * @x: multex 
+ * @x: multex
  * Return: result if whole or -1 otherwise
  */
 int square(int p, int x)

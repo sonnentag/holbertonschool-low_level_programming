6d72f59b2be300af9eb902eb54fb1b518d69367c
Tony Sims <zocle@zocle.net>
2020-03-01 21:56:04 +0000

    adding 3-mul.c to 0x0A-argc_argv subdir

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - multiply args
+ * @argc: - arg count
+ * @argv: - arg values
+ * Return: 0 for success or nothing given, 1 for non-numerical arg provided
+ */
+int main(int argc, char **argv)
+{
+	int mul, res = 0;
+
+	if (argc != 3)
+	{
+		printf("%s\n", "Error");
+		res = 1;
+	}
+	else
+	{
+		mul = atoi(argv[1]) * atoi(argv[2]);
+		printf("%i\n", mul);
+	}
+
+	return (res);
+}

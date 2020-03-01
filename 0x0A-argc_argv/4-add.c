04c26094970be0054cb3fe2959f83e594b60d58e
Tony Sims <zocle@zocle.net>
2020-03-01 21:56:17 +0000

    adding 4-add.c to 0x0A-argc_argv subdir

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *
+ *
+ *
+ */
+int main (int argc, char **argv)
+{
+
+int i, add = 0;
+
+for (i = 1; i < argc; i++)
+{
+if (!atoi(argv[i]))
+{
+printf("%s\n", "Error");
+return (1);
+}
+add += atoi(argv[i]);
+
+}
+printf("%d\n", add);
+return (0);
+}

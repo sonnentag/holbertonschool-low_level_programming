f23916f3aca6b8fa8c36c1eb2847ffce8ab3fc79
Tony Sims <zocle@zocle.net>
2020-03-05 20:09:27 +0000

    adding 0-malloc_checked.c to 0x0C-more_malloc_free subdir

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ *
+ *
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(b);
+	if (!ptr)
+		exit (98);
+
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+# holbertonschool-low_level_programming
+## 0x0C-more_malloc_free
+### 0-malloc_checked - malloc and check

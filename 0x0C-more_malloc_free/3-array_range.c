e2e676fb0f6efa9c199c11ed21ebbb7391bdf797
Tony Sims <zocle@zocle.net>
2020-03-06 04:15:32 +0000

    adding 3-array_range.c to 0x0C-more_malloc_free subdir

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * array_range - create a sequential array of ints within range
+ * @min: number to increment from
+ * @max: number to increment to
+ * Return: array on success, NULL on failure or if min > max
+ */
+int *array_range(int min, int max)
+{
+	int *arr;
+	int diff, n = 0;
+
+	if (min > max)
+		return (NULL);
+
+	diff = (max - min);
+	arr = malloc(diff * sizeof(*arr));
+	if (arr == NULL)
+		return (NULL);
+
+	while (n < diff)
+		arr[n++] = min++;
+
+	return (arr);
+}

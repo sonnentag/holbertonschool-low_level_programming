edfe0b3ea623de9cfb1cf244f754df5183450db6
Tony Sims <zocle@zocle.net>
2020-03-10 02:50:04 +0000

    adding dog.h to 0x0E-structures_typedef subdir

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG
+#define DOG
+
+/**
+ * struct dog - info needed for dogs
+ * @name: dog's anme
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif

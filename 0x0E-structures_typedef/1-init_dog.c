58949420ddc3384358e4bc670d842a834463eb18
Tony Sims <zocle@zocle.net>
2020-03-10 03:11:48 +0000

    adding 1-init_dog.c to 0x0E-structures_typedef subdir

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * init_dog - init dog struct
+ * @d: dog struct
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d == NULL)
+		;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
+}

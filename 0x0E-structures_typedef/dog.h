778a0079bf00f98e3cdd730dbd90e4f45b6a84f5
Tony Sims <zocle@zocle.net>
2020-03-10 04:24:33 +0000

    added free_dog proto

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG
-#define DOG
+#ifndef _DOG_H
+#define _DOG_H
 
 /**
  * struct dog - info needed for dogs
@@ -16,4 +16,9 @@ typedef struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+int _strlen(char *s);
+char *_strcpy(char *dest, char *src);
+void free_dog(dog_t *d);
+
 #endif

5833d060fd2e67aa8a240f74fedc0021af26b584
Tony Sims <zocle@zocle.net>
2020-03-13 02:24:08 +0000

    initial add of files for calc assignment

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,43 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - program to calculate arguments
+ * @argc: arg count
+ * @argv: arg values
+ * Return: 0 on success, 98-100 for various errors or failure.
+ */
+int main(int argc, char **argv)
+{
+	int i1, i2;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (!get_op_func(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	i1 = atoi(argv[1]);
+	i2 = atoi(argv[3]);
+
+	if (i2 == 0 && (*argv[3] == (char)37 || *argv[3] == (char)47))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	if (get_op_func(argv[2]) == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	printf("%d\n", (get_op_func(argv[2])(i1, i2)));
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,58 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * op_add - add 2 numbers
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - subtract 2nd number from first
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * op_mul - multiply 2 numbers
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - divide 1st number by 2nd
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - calculate modulus of 2nd number into 1st
+ * @a: 1st number
+ * @b: 2nd number
+ * Return: result
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}

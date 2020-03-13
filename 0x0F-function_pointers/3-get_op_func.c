847bca44d706a53ad7d1474650d6cfb945e168f6
Tony Sims <zocle@zocle.net>
2020-03-13 03:06:22 +0000

    changed comparison to use strcmp

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
-#include <stdlib.h>
-#include <stdio.h>
+#include <stddef.h>
+#include <string.h>
 
 /**
  * get_op_func - return function to calc based on sign given
@@ -22,9 +22,10 @@ int (*get_op_func(char *s))(int, int)
 	i = 0;
 	while (i < 5)
 	{
-		if (ops[i].op[0] == s[0])
+		if (strcmp(ops[i].op, s) == 0)
 			return (ops[i].f);
 		i++;
 	}
+
 	return (NULL);
 }

58d36ce578bddf860c57717027d7023fcfc0abb9
Tony Sims <zocle@zocle.net>
2020-03-16 18:36:06 +0000

    changed args to val in headers, added include stdarg, and made minor changes to 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,7 +1,45 @@
 #include <stdarg.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include "variadic_functions.h"
+
+/**
+  * op_c - prints characters
+  * @val: character to print
+  */
+void op_c(va_list val)
+{
+	printf("%c", va_arg(val, int));
+}
+
+/**
+  * op_i - prints integers
+  * @i: integer to print
+  */
+void op_i(va_list val)
+{
+	printf("%d", va_arg(val, int));
+}
+
+/**
+  * op_f - prints floats
+  * @f: float to print
+  */
+void op_f(va_list val)
+{
+	printf("%f", va_arg(val, double));
+}
+
+/**
+  * op_s - prints strings
+  * @s: string to print
+  */
+void op_s(va_list val)
+{
+	char *str = va_arg(val, char *);
+	if (!str)
+		str = "(nil)";
+	printf("%s", str);
+}
 
 /**
   * print_all - prints any argument passed into it
@@ -9,8 +47,8 @@
   */
 void print_all(const char * const format, ...)
 {
-	unsigned int i, j;
-	char *sep;
+	unsigned int i = 0, j;
+	char *sep = "";
 	va_list val;
 	va_ops ops[] = {
 		{"c", op_c},
@@ -20,65 +58,23 @@ void print_all(const char * const format, ...)
 		{NULL, NULL}
 	};
 
-	i = 0;
-	sep = "";
 	va_start(val, format);
 	while (format && format[i])
 	{
 		j = 0;
 		while (j < 4)
 		{
-			if (format[i] == *(ops[j].fmt))
+			if (format[i] == *ops[j].fmt)
 			{
 				printf("%s", sep);
 				ops[j].f(val);
 				sep = ", ";
+				break;
 			}
 			j++;
 		}
 		i++;
 	}
 	printf("\n");
-}
-
-/**
-  * op_c - prints characters
-  * @val: character to print
-  */
-void op_c(va_list arg)
-{
-	printf("%c", va_arg(arg, int));
-}
-
-/**
-  * op_i - prints integers
-  * @i: integer to print
-  */
-void op_i(va_list arg)
-{
-	printf("%d", va_arg(arg, int));
-}
-
-/**
-  * op_f - prints floats
-  * @f: float to print
-  */
-void op_f(va_list arg)
-{
-	printf("%f", va_arg(arg, double));
-}
-
-/**
-  * op_s - prints strings
-  * @s: string to print
-  */
-void op_s(va_list arg)
-{
-	char *str;
-
-	str = va_arg(arg, char *);
-	if (!str)
-		str = "(nil)";
-
-	printf("%s", str);
+	va_end(val);
 }
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,20 +1,28 @@
 #ifndef _VAR_FUNCS_H
 #define _VAR_FUNCS_H
 
+#include <stdarg.h>
+/**
+ * struct va_ops - specifier to func pointer
+ * @fmt: format specifier
+ * @f: func pointer to each
+ * va_ops: typedef
+ */
+typedef struct va_ops
+{
+	char *fmt;
+	void (*f)();
+} va_ops;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 
 void print_all(const char * const format, ...);
-void op_c(va_list args);
-void op_i(va_list args);
-void op_f(va_list args);
-void op_s(va_list args);
 
-typedef struct vops
-{
-	char *fmt;
-	void (*f)(va_list args);
-} va_ops;
+void op_c(va_list val);
+void op_i(va_list val);
+void op_f(va_list val);
+void op_s(va_list val);
 
 #endif

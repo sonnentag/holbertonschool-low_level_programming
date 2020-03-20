e4707ebdc545db0a977510e49b3041a37307436b
Tony Sims <zocle@zocle.net>
2020-03-20 03:21:46 +0000

    initial push of add_node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include <string.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - add a new node at the beginning of a list
+ * @head: beginning address
+ * @str: provided string
+ * Return: address pf new node, or NULL on failure
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_n;
+
+	if (!(head) || !(*str))
+		return (NULL);
+	new_n = malloc(sizeof(list_t));
+	if (!new_n)
+		return (NULL);
+	new_n->str  = strdup(str);
+	new_n->len = strlen(str);
+	new_n->next = *head;
+	*head    = new_n;
+
+	return (*head);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -2,3 +2,4 @@
 ## 0x12-singly_linked_lists
 ### 0-print_list.c - print the list in a struct
 ### 1-list_len.c - print the number of elements in a linked list
+### 2-add_node.c - add a new node at the beginning of a list 

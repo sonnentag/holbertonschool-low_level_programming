846b67aef3c8f1743319b7f4680aaf7cf1d668a4
Tony Sims <zocle@zocle.net>
2020-03-20 04:55:56 +0000

    fixed declaration to void

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,13 +5,10 @@ void start_f(void) __attribute__((constructor));
 int write(int filedes, const char *buf, unsigned int nbyte);
 
 /**
- * start_f - start constructor
- * Return: 0
+ * start_f - print text before main
  */
 void start_f(void)
 {
-	write(1, "You're beat! and yet, you must allow,\n", 37);
-	write(1, "I bore my house upon my back!\n", 29);
-
-	return (0);
+	write(1, "You're beat! and yet, you must allow,\n", 38);
+	write(1, "I bore my house upon my back!\n", 30);
 }

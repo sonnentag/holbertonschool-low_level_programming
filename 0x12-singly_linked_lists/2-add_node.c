ad40e67275c69180edbda73ee943b1916d9ac936
Tony Sims <zocle@zocle.net>
2020-03-20 03:44:56 +0000

    adjusted return vakue and added internal strlen func

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,23 @@
 #include "lists.h"
 
 /**
+ * _strlen - count characters in array
+ * @s: provided value in array
+ * Return: length of array
+ */
+int _strlen(const char *s)
+{
+	int counter = 0;
+
+	while (s[counter] != '\0')
+	{
+		counter++;
+	}
+
+	return (counter);
+}
+
+/**
  * add_node - add a new node at the beginning of a list
  * @head: beginning address
  * @str: provided string
@@ -15,10 +32,10 @@ list_t *add_node(list_t **head, const char *str)
 	new_n = malloc(sizeof(list_t));
 	if (!new_n)
 		return (NULL);
-	new_n->str  = strdup(str);
-	new_n->len = strlen(str);
+	new_n->str = strdup(str);
+	new_n->len = _strlen(str);
 	new_n->next = *head;
-	*head    = new_n;
+	*head = new_n;
 
-	return (head);
+	return (new_n);
 }

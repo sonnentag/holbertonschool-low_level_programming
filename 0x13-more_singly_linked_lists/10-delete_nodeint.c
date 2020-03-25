e6fb84bdaba7e884e245dc8a0fc5cc5f0602c9f9
Tony Sims <zocle@zocle.net>
2020-03-24 23:24:13 -0500

    initial commit of 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/*
+*/
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int c = 1;
+	listint_t *temp, *new_n;
+
+	if (!*head)	
+		return (-1);
+	if (index == 0)
+	{
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+
+		return (1);
+	}
+	new_n = *head;
+	while (c < index)
+	{
+		if (!new_n)
+			return (-1);
+
+		new_n = new_n->next;
+		c++;
+	}
+	temp = new_n->next;
+	new_n->next = temp->next;
+ 	free(temp);
+		
+	return (1);
+}

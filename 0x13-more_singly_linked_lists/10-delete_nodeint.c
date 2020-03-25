8ad69c1af388025b921beb5bc5b82a35f45a3a1e
Tony Sims <zocle@zocle.net>
2020-03-24 23:35:15 -0500

    added func and var descriptions to comments

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,14 +1,18 @@
 #include "lists.h"
 #include <stdlib.h>
 
-/*
-*/
+/**
+ * delete_nodeint_at_index - delete nth element from list
+ * @head: beginning of list
+ * @index: place of element to delete
+ * Return: 1 or -1 on failure 
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int c = 1;
 	listint_t *temp, *new_n;
 
-	if (!*head)	
+	if (!*head)
 		return (-1);
 	if (index == 0)
 	{
@@ -29,7 +33,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	}
 	temp = new_n->next;
 	new_n->next = temp->next;
- 	free(temp);
-		
+	free(temp);
+
 	return (1);
 }

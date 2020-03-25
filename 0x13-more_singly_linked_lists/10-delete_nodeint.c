b6694d903f0135992c27b43059c10ed366badda9
Tony Sims <zocle@zocle.net>
2020-03-24 23:36:35 -0500

    fixed extraneous whitespace per betty

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -5,7 +5,7 @@
  * delete_nodeint_at_index - delete nth element from list
  * @head: beginning of list
  * @index: place of element to delete
- * Return: 1 or -1 on failure 
+ * Return: 1 or -1 on failure
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {

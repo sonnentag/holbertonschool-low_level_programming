b1d450825857955d633133c32fe74a7955cf00c0
Tony Sims <zocle@zocle.net>
2020-03-27 03:16:08 +0000

    initial push of 4-clear_bit

---
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * clear_bit - sets the value of a bit to 0 at a given index
+ * @n: provided number
+ * @index: index to set
+ * Return: 1 or -1 on failure
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+  	if (index > 31)
+		return (-1);
+
+	*n &= -(1 << index);
+
+	return (1);
+}

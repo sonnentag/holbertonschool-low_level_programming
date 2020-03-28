fabcb3b6a70780e00c7cc2c91243626e34cb36fe
Tony Sims <zocle@zocle.net>
2020-03-28 04:56:11 +0000

    changed logic per pld discussion

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -7,63 +7,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	int l = _strlen(b);
-	int p = _pow_rec(2, l - 1);
-	int c = 0, d = 0;
-	unsigned int r = 0;
+	unsigned int c = 0, result = 0;
 
-	if (!b)
-		return (0);
+		while (b[c] != '\0')
+		{
+			if (b[c] != '0' && b[c] != '1')
+				return (0);
+			if (b[c] == '1')
+				result++;
+			if (b[c + 1] != '\0')
+				result = result << 1;
+			c++;
+		}
 
-	while (b[c])
-	{
-		d = b[c] - 48;
-		if (d != 0 && d != 1)
-			return (0);
-		r += d * p;
-		p = p / 2;
-		c++;
-	}
-
-	return (r);
-
-}
-
-/**
- * _strlen - count characters in array
- * @s: provided value in array
- * Return: length of array
- */
-int _strlen(const char *s)
-{
-	int c = 0;
-
-	while (s[c] != '\0')
-	{
-		c++;
-	}
-
-	return (c);
-}
-
-/**
- * _pow_rec - return value of x raised to y
- * @x: provided
- * @y: provided
- * Return: -1 if negative, 1 if 0, otherwise result
- */
-int _pow_rec(int x, int y)
-{
-	int n;
-
-	if (x == 0)
-		return (0);
-	if (y < 0)
-		return (-1);
-	if (y == 0)
-		return (1);
-
-	n = (x * _pow_rec(x, y - 1));
-
-	return (n);
+	return (result);
 }

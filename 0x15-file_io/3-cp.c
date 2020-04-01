9904b20c01d3fda0cc3856e4513147a5ed37fcd2
Tony Sims <zocle@zocle.net>
2020-04-01 07:27:16 +0000

    fixed target exit 99

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int argc, char **argv)
 
 	t = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (t == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	while ((r = read(s, buf, BUFSIZ >> 3)))
 	{

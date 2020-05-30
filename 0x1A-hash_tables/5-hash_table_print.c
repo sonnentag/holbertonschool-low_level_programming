9b5bddc4b0270302727ba46863862e2eb32935e1
Tony Sims <zocle@zocle.net>
2020-05-30 04:54:20 +0000

    pushing 5

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - print a hash table
+ * @ht: hash table to print
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int idx = 0;
+	hash_node_t *tmparr = NULL;
+	int start = 0;
+
+	if (!ht)
+		return;
+
+	printf("{");
+	while (idx < ht->size)
+	{
+		tmparr = ht->array[idx];
+		while (tmparr)
+		{
+			if (start == 1)
+				printf(", ");
+			start = 1;
+			printf("'%s' : '%s'", tmparr->key, tmparr->value);
+			tmparr = tmparr->next;
+		}
+		idx++;
+	}
+	printf("}\n");
+}
--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -40,5 +40,7 @@ unsigned long int key_index(const unsigned char *key, unsigned long int size);
 int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 /* 4 */
 char *hash_table_get(const hash_table_t *ht, const char *key);
+/* 5 */
+void hash_table_print(const hash_table_t *ht);
 
 #endif

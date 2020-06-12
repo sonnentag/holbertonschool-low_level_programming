bedc9f45072c8aea466e91c5fe803bc50d507081
Tony Sims <zocle@zocle.net>
2020-06-12 15:39:13 +0000

    pushing delete

--- 0x1A-hash_tables/hash_tables.h
+++ 0x1A-hash_tables/hash_tables.h
@@ -42,5 +42,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value);
 char *hash_table_get(const hash_table_t *ht, const char *key);
 /* 5 */
 void hash_table_print(const hash_table_t *ht);
+/* 6 */
+void hash_table_delete(hash_table_t *ht);
 
 #endif
